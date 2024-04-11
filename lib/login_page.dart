81fb3f2e07556a364c72e9ce0eb2222a5fc02879
dreichi <damien.jenger@gmail.com>
2024-04-11 11:55:23 +0200

    Login/singup done

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,30 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
-  void signInWithGoogle(BuildContext context) async {
-    final response = await Supabase.instance.client.auth.signInWithOAuth(
-      OAuthProvider.google
-    );
-
-    if (response == false ) {
-      ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text('Failed to sign in with Google')));
-    } else {
-      Navigator.of(context).pushReplacementNamed('/');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      appBar: AppBar(
+        title: const Text('Sign Up / Login'),
+      ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => signInWithGoogle(context),
-          child: Text('Sign in with Google'),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SupaEmailAuth(
+                onSignInComplete: (response) {
+                  if (response.user != null) {
+                    Navigator.of(context).pushReplacementNamed('/main');
+                  }
+                },
+                onSignUpComplete: (response) {
+                  if (response.user != null) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Inscription réussie'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: const <Widget>[
+                                Text('Veuillez vérifier votre e-mail pour activer votre compte.'),
+                              ],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                },
+                metadataFields: [
+                  MetaDataField(
+                    prefixIcon: const Icon(Icons.person),
+                    label: 'Nom',
+                    key: 'name',
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Rentrez quelque chose';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+
+              SupaSocialsAuth(
+                socialProviders: [
+                  OAuthProvider.google,
+                ],
+                colored: true,
+                onSuccess: (Session response) {
+                  Navigator.of(context).pushReplacementNamed('/main');
+                },
+                onError: (error) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Authentication error: $error')),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    await Future.delayed(Duration(seconds:3));
+    if (!mounted) {
+      return;
+    }
+
+    final session = supabase.auth.currentSession;
+    if (session != null) {
+      Navigator.of(context).pushReplacementNamed('/main');
+    } else {
+      Navigator.of(context).pushReplacementNamed('/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child:             CircularProgressIndicator(color: Colors.black,),
+        ),
+      ),
+    );
+  }
+}
